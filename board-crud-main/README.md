a6002b5cfabc74b8cacd0bff138a8c8cae5f0883
hyojinkim830 <159021949+hyojinkim830@users.noreply.github.com>
2024-11-27 14:39:25 +0900

    delete

--- board-crud-main/README.md
+++
@@ -1,2 +0,0 @@
-# board-crud
-Reactjs + Spring boot + MySQL 연동한 게시글 CRUD
--- 김효진/SPRING REACT MYSQL/board-front/src/types/enum/index.ts
+++
@@ -1,6 +0,0 @@
-import ResponseCode from "./response-code.enums";
-
-export default interface ResponseDto{
-    code: ResponseCode;
-
-}
